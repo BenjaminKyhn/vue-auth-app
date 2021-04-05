73db3323e3fb6d571b58367b9ebd3acfc7fc27d6
Benjamin Kyhn <benkyhn@hotmail.com>
2021-04-05 10:03:14 +0200

    Initial commit

---
+++ frontend/src/components/Messages.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h4 class="display-1">Messages</h4>
+        <v-list>
+            <v-list-tile v-for="(message, i) in messages" :key="i">
+                <v-list-tile-content>
+                    <v-list-item v-text="message"></v-list-item>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                messages: ["Hello", "Hi", "Derp"]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
