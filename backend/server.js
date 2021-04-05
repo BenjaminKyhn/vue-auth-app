3224cdb2fff462731e58b44cf2d4fed5d4750991
Benjamin Kyhn <benkyhn@hotmail.com>
2021-04-05 10:07:00 +0200

    Part 3-2

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('test'));
+app.get('/', (req, res) => {
+    const messages = ["Hello", "Hi", "Derp"];
+
+    res.send(messages);
+});
 
 app.listen(port, () => console.log("app running"));
